4e5111044c0cef43465509cf5c60d36a07cf9aaa
MarcusNyne <69087098+MarcusNyne@users.noreply.github.com>
2024-02-18 22:57:25 -0500

    Revert "changed enabled visual"
    This reverts commit 59ed6c394896aef9e7df833790d7760fe26da9fc.

--- javascript/tweak_weights_code.js
+++ javascript/tweak_weights_code.js
@@ -4,32 +4,29 @@ function m9_tweak_weights_tolower(str) {
   return "";
 }
 
-function m9_tweak_weights_find_child(ele, tag, cls=null, text=null) {
+function m9_tweak_weights_find_child(ele, tag, cls=null) {
   tag = m9_tweak_weights_tolower(tag);
   // console.log ("Parent: "+String(ele));
   for (let i = 0; i < ele.childNodes.length; i++) {
     // console.log(String(i)+": "+String(ele.childNodes[i])+" > "+m9_tweak_weights_tolower(ele.childNodes[i].tagName));
     // console.log(String(i)+": "+String(ele.childNodes[i])+" # "+String(ele.childNodes[i].classList));
-    cit = ele.childNodes[i].innerText
-    if (m9_tweak_weights_tolower(ele.childNodes[i].tagName)==tag
-          && (cls === null || ele.childNodes[i].classList.contains(cls))
-          && (text === null || ((typeof cit)=='string' && cit.includes(text))))
+    if (m9_tweak_weights_tolower(ele.childNodes[i].tagName)==tag &&
+          (cls === null || ele.childNodes[i].classList.contains(cls)))
       return ele.childNodes[i];
   }
   return null;
 }
 
 m9_tweak_weights_accordion_element = null;
-m9_tweak_weights_accordion_tag = null;
 m9_tweak_weights_check_element = null;
 
 function m9_tweak_weights_update_enabled(event) {
   // console.log("checked: " + String(m9_tweak_weights_check_element.checked))
   if (m9_tweak_weights_check_element!=null) {
     if (m9_tweak_weights_check_element.checked)
-      m9_tweak_weights_accordion_tag.classList.add("enabled");
+      m9_tweak_weights_accordion_element.classList.add("enabled");
     else
-      m9_tweak_weights_accordion_tag.classList.remove("enabled");
+      m9_tweak_weights_accordion_element.classList.remove("enabled");
   }
 }
 
@@ -44,16 +41,6 @@ function m9_tweak_weights_loaded() {
   {
     // console.log("got both")
     m9_tweak_weights_accordion_element = m9_tweak_weights_find_child(m9_tweak_weights_accordion, "div", "label-wrap");
-    if (m9_tweak_weights_accordion_element!=null) {
-      span = m9_tweak_weights_find_child(m9_tweak_weights_accordion_element, "span", null, "M9");
-      // console.log("span: "+String(span))
-      if (span!=null) {
-        m9_tweak_weights_accordion_tag = document.createElement("div");
-        m9_tweak_weights_accordion_tag.className = 'm9-tweak-weights-tag';
-        m9_tweak_weights_accordion_tag.innerText = 'enabled';
-        span.appendChild(m9_tweak_weights_accordion_tag);
-      }
-    }
     // console.log("m9_tweak_weights_accordion_element: "+String(m9_tweak_weights_accordion_element));
     
     ele = m9_tweak_weights_find_child(m9_tweak_weights_enabled, "label");
